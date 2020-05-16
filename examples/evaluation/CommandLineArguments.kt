a80848d3aeed3db6f550a0eacfaa2cd6cc72fd31
Simone Cangialosi <sm.cangialosi@gmail.com>
2020-05-16 18:42:50 +0200

    Update command description

--- examples/evaluation/CommandLineArguments.kt
+++ examples/evaluation/CommandLineArguments.kt
@@ -22,12 +22,12 @@ internal class CommandLineArguments(args: Array<String>) {
   private val parser = ArgParser(args)
 
   /**
-   * The file path of the serialized text frames extractor model.
+   * The file path of the serialized text morpho predictor model.
    */
   val modelPath: String by parser.storing(
     "-m",
     "--model-path",
-    help="the file path of the serialized text frames predictor model"
+    help="the file path of the serialized text morpho predictor model"
   )
 
   /**
